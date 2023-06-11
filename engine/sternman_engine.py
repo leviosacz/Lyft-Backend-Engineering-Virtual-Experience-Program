c46d343ea7b9304ed1dce8e1c382fb243943f17f
leviosacz <c.expecto.patronum@gmail.com>
2023-06-11 15:18:20 -0700

    Update sternman engine

--- engine/sternman_engine.py
+++ engine/sternman_engine.py
@@ -6,4 +6,4 @@ class SternmanEngine(Car, ABC):
         self.warning_light_on = warning_light_on
 
     def needs_service(self):
-        return self.warning_light_on:
+        return self.warning_light_on
