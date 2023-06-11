2a73d9db6a0410698ef1bb83b4f0d614cf47fbb5
leviosacz <c.expecto.patronum@gmail.com>
2023-06-11 01:14:32 -0700

    Update sternman engine

--- engine/engine_type/sternman_engine.py
+++ engine/engine_type/sternman_engine.py
@@ -2,8 +2,8 @@ from engine.engine import Engine
 
 
 class SternmanEngine(Car, ABC):
-    def __init__(self, warning_light_is_on):
-        self.warning_light_is_on = warning_light_is_on
+    def __init__(self, warning_light_on):
+        self.warning_light_is_on = warning_light_on
 
     def engine_should_be_serviced(self):
-        return self.warning_light_is_on:
+        return self.warning_light_on:
